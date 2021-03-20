8b84d12d3c2ee8056c2c65bbd63054f33e75ea7a
simPod <simon@podlipsky.net>
2021-03-20 14:14:46 +0000

    Deploying to gh-pages from  @ de4f910600452782840d694e4561f76a09408361 🚀

---
+++ _next/static/1KQTee1IZ6WyvCYXVdyNX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F07f5c300c6e4b3bbfbdc.css","static\u002Fchunks\u002Fpages\u002Findex-fa72caac70815c4dbe13.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8b2f3807128f90b98724.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
